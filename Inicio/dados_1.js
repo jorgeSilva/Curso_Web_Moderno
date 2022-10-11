118f7e99650fb3aaf905b212a76de0e2896fc389
Jorge Silva <jorgelfsilva5@gmail.com>
2022-10-11 12:33:02 -0300

    [aula23] Trabalhando com dados

---
+++ Inicio/dados_1.js
@@ -0,0 +1,11 @@
+let quantidade = 10; 
+let preco = 6.4
+let imposto = 1.5
+let precoFinal = preco + imposto
+let nome = "caneta"
+
+console.log(nome);
+console.log(quantidade);
+console.log(preco);
+console.log(imposto);
+console.log(precoFinal);
