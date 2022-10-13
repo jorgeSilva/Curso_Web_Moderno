b344c1a584f9d04e972aeb0779bb7c3942097b3a
Jorge Silva <jorgelfsilva5@gmail.com>
2022-10-13 09:06:00 -0300

    [aula25] Trabalhando com dados e a funcionalidade 'typeof' para descobrir o tipo dos dados

---
+++ Inicio/dados_3.js
@@ -0,0 +1,10 @@
+let idade = 31;
+let nome = "Aldair"
+let salario = 1212.90
+let estaChovendo = false;
+
+console.log(typeof 31);
+console.log(typeof idade);
+console.log(typeof nome);
+console.log(typeof salario);
+console.log(typeof estaChovendo)
