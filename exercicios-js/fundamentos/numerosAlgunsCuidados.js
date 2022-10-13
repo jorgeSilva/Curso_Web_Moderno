51d943c4d12f9773fb5177b6f9e82181e5411097
Jorge Silva <jorgelfsilva5@gmail.com>
2022-10-13 12:21:21 -0300

    [aula41] Algumas dicas para se atentar e diferenças do JavaScript

---
+++ exercicios-js/fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,8 @@
+console.log(7 / 0)
+console.log("10" / 2)
+console.log("10.4" / 2)
+console.log("Show!" * 2)
+console.log(0.1 + 0.7)
+//console.log(10.toString())
+console.log((10.738723).toFixed(2))
+console.log((10.738723).toString())
