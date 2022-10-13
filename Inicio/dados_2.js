784074080a994b97297eabaac0fa0cdb0dca8dc5
Jorge Silva <jorgelfsilva5@gmail.com>
2022-10-13 08:54:30 -0300

    [aula24] Trabalhando com dados e template string

---
+++ Inicio/dados_2.js
@@ -0,0 +1,7 @@
+let preco = 19.90;
+let desconto = 0.4;
+let precoComDesconto = preco * (1 - desconto);
+let nome = "Caderno";
+let categoria = "Papelaria";
+
+console.log(` Produto: ${nome}\n Categoria:${categoria}\n Preço: ${preco}\n Desconto: ${desconto}\n Preço com desconto: ${precoComDesconto} `)
