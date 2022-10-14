4143320a08a399edfb8f1716fdbd9b4eaf5187b8
Jorge Silva <jorgelfsilva5@gmail.com>
2022-10-14 10:45:12 -0300

    [aula44] Utilizando template string, fazendo operações matematicas junto de funções

---
+++ exercicios-js/fundamentos/templateString.js
@@ -0,0 +1,10 @@
+const nome = 'João'
+const concatenacao = 'Ola' + nome + '!'
+const template = `Olá ${nome} !`
+
+console.log(concatenacao, template)
+
+console.log(` 1 + 1 = ${1 + 1}`)
+
+const up = texto => texto.toUpperCase()
+console.log(`Ei... ${up('cuidado')} !`)
