8f007ecef30bb430a53c581b04f28e8614dfa72a
Jorge Silva <jorgelfsilva5@gmail.com>
2022-10-19 15:14:27 -0300

    [aula47] Anatomia do objeto junto a inserção de dados dinamicamente

---
+++ exercicios-js/fundamentos/objeto.js
@@ -0,0 +1,17 @@
+const prod1 = {}
+
+prod1.nome = 'Celular ultra Mega'
+prod1.preco = 4998.5
+
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Nitro 5 AN515-54',
+    preco: 5499.90,
+    obj: {
+        celular: 'iPhone 14 pro',
+        preco: 'muitos'
+    }
+}
+
+console.log(prod2)
