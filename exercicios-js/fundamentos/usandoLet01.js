ba49222f9aac83a82af7d275edc20ef9e59d39c8
Jorge Silva <jorgelfsilva5@gmail.com>
2022-10-28 09:52:58 -0300

    [aula54] Vendo o funcionamento de  variaveis do tipo let em escopos globais, funções e de blocos

---
+++ exercicios-js/fundamentos/usandoLet01.js
@@ -0,0 +1,17 @@
+let numero = 1 
+
+{
+    let numero = 2
+    console.log('drento = ', numero)
+}
+
+console.log('fora =', numero)
+
+let number = 4
+
+{
+    let numberTwo = 5
+    console.log(number)
+}
+
+console.log(number)
