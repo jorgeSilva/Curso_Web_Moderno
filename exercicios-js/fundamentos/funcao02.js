d3b31cba20ca208c18578365d92f353cfb2e7bff
Jorge Silva <jorgelfsilva5@gmail.com>
2022-10-28 08:12:02 -0300

    [aula51] Exibindo como pode ser reduzida uma função

---
+++ exercicios-js/fundamentos/funcao02.js
@@ -0,0 +1,17 @@
+const imprimirSoma = function(a, b){
+    console.log(a + b)
+}
+
+imprimirSoma(2, 3)
+
+const soma = (a, b) => {
+    return a + b 
+}
+
+console.log(soma(2,2))
+
+const subtracao = (a, b ) => a - b 
+console.log(subtracao(3, 7))
+
+const imprimir2 = a => console.log(a)
+imprimir2('legal')
