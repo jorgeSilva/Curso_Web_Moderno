acaf85810ccd5cc0c80f8c4fec1ce4f6c3c77636
Jorge Silva <jorgelfsilva5@gmail.com>
2022-12-27 14:59:08 -0300

    [aula77] Passando somente pela estrutura de controle 'IF', percebendo que é executado somente se a condição for== true

---
+++ exercicios-js/controle/if_02.js
@@ -0,0 +1,16 @@
+function teste1(nota){
+    if(nota > 7) 
+        console.log(`Passou com ${nota}`)
+    console.log(`Quase reprovou`);
+}
+
+teste1(6)
+teste1(8)
+
+function teste2(nota){
+    if(nota);{
+        console.log(`Sua nota foi ${nota}`);
+    }
+}
+
+teste2(7)
