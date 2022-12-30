0511343af7f85e32ddd6c90324027dcb8bb569b1
Jorge Silva <jorgelfsilva5@gmail.com>
2022-12-30 08:05:32 -0300

    [aula86] Acessando os valores e indices de arrays e objetos atraves com o 'FOR/IN'

---
+++ exercicios-js/controle/for2.js
@@ -0,0 +1,16 @@
+const notas = [8, 1, 6, 7]
+
+for(i in notas){
+    console.log(i, notas[i])
+}
+
+const nomes = {
+    nome:'Ana',
+    sobrenome: 'Ferreira',
+    idade: 49,
+    peso: 88
+}
+
+for(i in nomes){
+    console.log(i, nomes[i])
+}
