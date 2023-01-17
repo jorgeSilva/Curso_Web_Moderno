32abcfc7bc13aa391341e8245de945b4dad4dcf4
Jorge Silva <jorgelfsilva5@gmail.com>
2023-01-17 09:47:11 -0300

    [aula134] Vendo como se utiliza o metodo '.foreach()' em arrays.

---
+++ exercicios-js/array/foreach01.js
@@ -0,0 +1,12 @@
+const aprovados = ['Agtha', 'Aldo', 'Daniel', 'Raquel']
+
+aprovados.forEach(function(nome, indice){
+    console.log(`${indice + 1} ${nome}`)
+})
+console.log("--------------------------");
+
+aprovados.forEach(nome => console.log(nome))
+
+console.log("--------------------------");
+const exibirAprovados = aprovados => console.log(aprovados);
+aprovados.forEach(exibirAprovados)
