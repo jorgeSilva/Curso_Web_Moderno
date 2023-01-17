668d25d7d3e7dda00a5051a4bfde54e83c0cb8f0
Jorge Silva <jorgelfsilva5@gmail.com>
2023-01-17 12:02:42 -0300

    [aula146] Vendo o metodo '.concat()' e atribuindo elementos por ele.

---
+++ exercicios-js/array/concat.js
@@ -0,0 +1,7 @@
+const filhas = ['Ualeska', 'Cibalema']
+const filhos = ['Taldo', 'Gerson']
+const todos = filhas.concat(filhos, 'Fulano')
+
+console.log(todos);
+
+console.log([].concat([1,2], [3, 4], 5, [[6, 7]]));
