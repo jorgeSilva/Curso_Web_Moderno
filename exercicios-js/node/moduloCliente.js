6b19bc04ee2347e57415f33051427aa4905399ca
Jorge Silva <jorgelfsilva5@gmail.com>
2023-01-18 09:32:04 -0300

    [aula150] Aprendendo sobre o node e como exportar e consumir os modulos que achamos necessario.

---
+++ exercicios-js/node/moduloCliente.js
@@ -0,0 +1,13 @@
+const moduloA = require('./moduloA')
+const moduloB = require('./moduloB.js')
+
+console.log(moduloA);
+console.log(moduloA.ola);
+console.log(moduloA.bemVindo);
+console.log(moduloA.ateLogo);
+
+console.log("-------------------");
+
+console.log(moduloB);
+console.log(moduloB.boaNoite());
+console.log(moduloB.bomDia);
